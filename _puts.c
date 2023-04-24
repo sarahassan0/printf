6e90fbf8be91384ea1a2565a79050d37468af2b4
ArwaMagdi <arwamagdi20@gmail.com>
2023-04-24 15:25:26 -0700

    Updated

---
+++ _puts.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+int _puts(char *str)
+{
+        int i = 0;
+
+        while (str[i] != '\0')
+        {
+                _putchar(str[i]);
+                i++;
+        }
+        _putchar('\n');
+        return (i + 1);
+}
+
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
