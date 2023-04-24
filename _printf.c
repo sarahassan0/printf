3877e73122e425203bee1959c884c58acf91f92f
Sarah Hassan <sarraree5@gmail.com>
2023-04-24 15:20:11 -0700

    main.h , _printf

---
+++ _printf.c
@@ -0,0 +1,61 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0;
+	va_list args;
+
+	if (!format)
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+
+			switch (format[i])
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
+
+					printed = _putchar(c);
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char *);
+
+					printed = _puts(s);
+					break;
+				}
+				case '%':
+				{
+					_putchar('%');
+					printed++;
+					break;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			printed++;
+		}
+	}
+
+	va_end(args);
+
+	return (printed);
+}
+
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+
+int _printf(const char *format, ...);
+int _puts(char *str);
+int _putchar(int c);
+
