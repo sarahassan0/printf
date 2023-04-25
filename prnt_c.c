d3d628c808bb28fa595aa3af89d54a65b959b068
ArwaMagdi <arwamagdi20@gmail.com>
2023-04-26 00:25:51 +0200

    Updated

---
+++ prnt_c.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prnt_c - Prints a character
+ *
+ * @args: A va_list containing the character to print
+ *
+ * Return: The number of characters printed
+ */
+
+int prnt_c(va_list *args)
+{
+	int prntd = 0;
+
+	prntd += _putchar(va_arg(*args, int));
+
+	return (prntd);
+} 
