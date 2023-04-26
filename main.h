0132f1427fc66a9cec7090bd8cf849fc96fb3022
Sarah Hassan <sarraree5@gmail.com>
2023-04-25 19:19:41 -0700

    update

--- main.h
+++ main.h
@@ -5,7 +5,6 @@
 #include <unistd.h>
 
 
-
 int _printf(const char *format, ...);
 int _puts(char *str);
 int _putchar(char c);
@@ -13,9 +12,7 @@ int get_precision(const char *format, va_list *args);
 int prnt_c(va_list *args);
 int prnt_str(va_list *args);
 int prnt_int(va_list *args);
-int convert2Hex(char hex[]);
-int checkOctal(int octal);
-int convertOctal2Decimal(int octal);
+int prnt_d(int d);
 
 #endif /* MAIN_H */
 
